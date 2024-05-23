515b97bbdd7d3cc2fe227bb31b74d020139b9699
Kelechukwu <kelebrizzle@gmail.com>
2024-05-23 12:11:11 -0700

    fix checker error

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,27 +3,17 @@ const util = require('util');
 const request = util.promisify(require('request'));
 const filmID = process.argv[2];
 
-async function starwarsCharacters(filmId) {
-  try {
-    const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
-    const response = await request(endpoint);
-    if (!response.statusCode === 200) {
-      throw new Error(`Error fetching film data: ${response.statusCode}`);
-    }
-    const data = JSON.parse(response.body);
-    const characters = data.characters;
+async function starwarsCharacters (filmId) {
+  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+  let response = await (await request(endpoint)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
 
-    for (let i = 0; i < characters.length; i++) {
-      const urlCharacter = characters[i];
-      const characterResponse = await request(urlCharacter);
-      if (!characterResponse.statusCode === 200) {
-        throw new Error(`Error fetching character data: ${characterResponse.statusCode}`);
-      }
-      const characterData = JSON.parse(characterResponse.body);
-      console.log(characterData.name);
-    }
-  } catch (error) {
-    console.error(error.message);
+  for (let i = 0; i < characters.length; i++) {
+    const urlCharacter = characters[i];
+    let character = await (await request(urlCharacter)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
   }
 }
 
